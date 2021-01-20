3d99e7700006ddcff557e20af8950db2faa5d598
Victor Mesquita <mesquitadev@gmail.com>
2021-01-20 18:32:22 -0300

    M - Routes - Add Initial Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {View, Text, StatusBar} from 'react-native';
+import {View, StatusBar} from 'react-native';
+
+import Routes from './routes';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App: React.FC = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#40CCB2" />
       <View style={{flex: 1, backgroundColor: '#40CCB2'}}>
-        <Text>Template Inicial</Text>
+        <Routes />
       </View>
-    </>
+    </NavigationContainer>
   );
 };
 

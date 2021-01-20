2ae01203efb6528de73119fc8177340e0b13af35
Victor Mesquita <mesquitadev@gmail.com>
2021-01-20 18:33:22 -0300

    M - Pages - Add SignIn and SignUp Structure pages

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image} from 'react-native';
 
-import {Container} from './styles';
+import {
+  Container,
+  BoxLogo,
+  ContainerData,
+  ContainerText,
+  ContainerButtons,
+  LightTitle,
+  BoldTitle,
+} from './styles';
+
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Text>Teste</Text>
-    </Container>
+    <>
+      <Container>
+        <BoxLogo>
+          <Image source={logoImg} />
+        </BoxLogo>
+        <ContainerData>
+          <ContainerText>
+            <LightTitle>Seja bem-vindo.</LightTitle>
+            <BoldTitle>O que deseja fazer?</BoldTitle>
+          </ContainerText>
+        </ContainerData>
+        <ContainerButtons>
+          <Button disabled>Entrar</Button>
+          <Button>Registrar</Button>
+        </ContainerButtons>
+      </Container>
+    </>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,3 +1,39 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
+export const BoxLogo = styled.View`
+  padding: 50px;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+`;
+export const ContainerData = styled.View`
+  padding-top: 30px;
+  align-items: center;
+  margin: 10px 20px 20px;
+`;
+
+export const ContainerText = styled.View`
+  align-self: flex-start;
+  margin: 0 20px 0;
+`;
+
+export const ContainerButtons = styled.View`
+  padding: 0 30px;
+`;
+
+export const LightTitle = styled.Text`
+  align-self: flex-start;
+  font-size: 20px;
+  color: white;
+  font-family: 'Poppins-Regular';
+`;
+
+export const BoldTitle = styled.Text`
+  align-self: flex-start;
+  font-size: 20px;
+  color: white;
+  font-family: 'Poppins-Bold';
+`;
